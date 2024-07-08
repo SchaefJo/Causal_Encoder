4d22706d864b894c9a20a58ce6bcdf36003c4bcd
Jonas <jonas.schaefer98@gmail.com>
2024-07-08 20:01:21 +0200

    remove debug prints

--- models/vae/lightning_module.py
+++ models/vae/lightning_module.py
@@ -94,10 +94,8 @@ class VAE(pl.LightningModule):
 
         # Encoder-Decoder init
         if self.hparams.no_encoder_decoder:
-            print('case1')
             self.encoder, self.decoder = nn.Identity(), nn.Identity()
         elif self.hparams.linear_encoder_decoder:
-            print('case2')
             nn_hid = max(512, 2 * self.hparams.c_hid)
             self.encoder = nn.Sequential(
                 nn.Linear(self.hparams.c_in, nn_hid),
@@ -116,7 +114,6 @@ class VAE(pl.LightningModule):
             )
         else:
             if self.hparams.img_width == 32:
-                print('case3')
                 self.encoder = SimpleEncoder(c_in=self.hparams.c_in,
                                              c_hid=self.hparams.c_hid,
                                              num_latents=self.hparams.num_latents)
@@ -124,7 +121,6 @@ class VAE(pl.LightningModule):
                                              c_hid=self.hparams.c_hid,
                                              num_latents=self.hparams.num_latents)
             else:
-                print('case4')
                 self.encoder = Encoder(num_latents=self.hparams.num_latents,
                                        c_hid=self.hparams.c_hid,
                                        c_in=self.hparams.c_in,
