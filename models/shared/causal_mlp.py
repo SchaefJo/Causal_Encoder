a88be1eeed562c7d2e00892c11dd8f5e398aadc8
Jonas <jonas.schaefer98@gmail.com>
2024-07-09 21:01:50 +0200

    fix pred issue

--- models/shared/causal_mlp.py
+++ models/shared/causal_mlp.py
@@ -103,8 +103,7 @@ class CausalMLP(nn.Module):
 
             for i, causal in enumerate(categorical_causal):
                 ce = F.cross_entropy(categorical_pred[:, i], categorical_target[:, i])
-                categorical_pred_labels = torch.argmax(categorical_pred[:, i], dim=0)
-                acc = accuracy_score(categorical_target[:, i].cpu(), categorical_pred_labels.cpu())
+                acc = accuracy_score(categorical_target[:, i].cpu(), categorical_pred[:, i].cpu())
                 individual_losses.append({'causal': causal, 'split': split, 'loss': ce.item(), 'accuracy': acc})
 
         self._save_metrics_to_file(individual_losses, self.results_path)
